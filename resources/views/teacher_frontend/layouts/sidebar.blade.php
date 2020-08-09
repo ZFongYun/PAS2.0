d96a2da3b7c9dd31d97e460eb5610eb9de8c45e0
Fong-Yun Zhang <4a790071@stust.deu.tw>
2020-08-10 02:02:33 +0800

    導覽列設置

--- resources/views/teacher_frontend/layouts/sidebar.blade.php
+++ resources/views/teacher_frontend/layouts/sidebar.blade.php
@@ -9,73 +9,44 @@
 
             <li class="mt">
                 <a href="index.html">
-                    <i class="fa fa-dashboard"></i>
-                    <span>Dashboard</span>
+                    <i class="fa fa-home"></i>
+                    <span>首頁</span>
                 </a>
             </li>
 
             <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-desktop"></i>
-                    <span>UI Elements</span>
+                <a href="index.html">
+                    <i class="fa fa-cog"></i>
+                    <span>會議管理</span>
                 </a>
-                <ul class="sub">
-                    <li><a  href="general.html">General</a></li>
-                    <li><a  href="buttons.html">Buttons</a></li>
-                    <li><a  href="panels.html">Panels</a></li>
-                </ul>
             </li>
 
             <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-cogs"></i>
-                    <span>Components</span>
+                <a href="index.html">
+                    <i class="fa fa-user"></i>
+                    <span>成員名單</span>
                 </a>
-                <ul class="sub">
-                    <li><a  href="calendar.html">Calendar</a></li>
-                    <li><a  href="gallery.html">Gallery</a></li>
-                    <li><a  href="todo_list.html">Todo List</a></li>
-                </ul>
             </li>
+
             <li class="sub-menu">
-                <a class="active" href="javascript:;" >
-                    <i class="fa fa-book"></i>
-                    <span>Extra Pages</span>
+                <a href="index.html">
+                    <i class="fa fa-users"></i>
+                    <span>小組專區</span>
                 </a>
-                <ul class="sub">
-                    <li class="active"><a  href="blank.html">Blank Page</a></li>
-                    <li><a  href="login.html">Login</a></li>
-                    <li><a  href="lock_screen.html">Lock Screen</a></li>
-                </ul>
             </li>
+
             <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-tasks"></i>
-                    <span>Forms</span>
+                <a href="index.html">
+                    <i class="fa fa-search"></i>
+                    <span>學生成績</span>
                 </a>
-                <ul class="sub">
-                    <li><a  href="form_component.html">Form Components</a></li>
-                </ul>
             </li>
+
             <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class="fa fa-th"></i>
-                    <span>Data Tables</span>
+                <a href="#">
+                    <i class="fa fa-download"></i>
+                    <span>報告下載</span>
                 </a>
-                <ul class="sub">
-                    <li><a  href="basic_table.html">Basic Table</a></li>
-                    <li><a  href="responsive_table.html">Responsive Table</a></li>
-                </ul>
-            </li>
-            <li class="sub-menu">
-                <a href="javascript:;" >
-                    <i class=" fa fa-bar-chart-o"></i>
-                    <span>Charts</span>
-                </a>
-                <ul class="sub">
-                    <li><a  href="morris.html">Morris</a></li>
-                    <li><a  href="chartjs.html">Chartjs</a></li>
-                </ul>
             </li>
 
         </ul>
