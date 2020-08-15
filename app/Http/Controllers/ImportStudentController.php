fa55dfb09a3143f5edb95d53e09417cc62c0d488
Fong-Yun Zhang <4a790071@stust.deu.tw>
2020-08-16 01:24:27 +0800

    新增ImportStudentController

---
+++ app/Http/Controllers/ImportStudentController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Imports\ImportContacts;
+use App\Models\Student;
+use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+//use Illuminate\Support\Facades\Hash;
+
+class ImportStudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $student = Student::all()->toArray();
+        $total = Student::all()->count();
+        return view('teacher_frontend.ImportStudent',compact('student','total'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+//        $password = $request->input('password');
+//        $student = new Student();
+//        $student->password = Hash::make($password);
+//        $student->save();
+
+        $request->validate([
+            'import_file' => 'required'
+        ]);
+        Excel::import(new ImportContacts, request()->file('import_file'));
+
+        return back()->with('success', '加入成功！');
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+        $student -> delete();
+        return redirect('ImportStudent');
+    }
+}
