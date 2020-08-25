e848f9d1c217ee448e2656c380effca21bda6bba
Fong-Yun Zhang <4a790071@stust.deu.tw>
2020-08-26 00:18:37 +0800

    新增編輯會議功能

--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -99,7 +99,15 @@ class MeetingController extends Controller
      */
     public function edit($id)
     {
-        return $id;
+        $meeting = Meeting::find($id) -> toArray();
+//        $team = Team::all('id','name')->toArray();
+        $team_chk = $meeting['report_team'];
+        $team_chk_arr = explode(" ",$team_chk);
+        $team_chk_length = count($team_chk_arr);
+        $team = Team::all()->toArray();
+        $team_length = count($team);
+
+        return view('teacher_frontend.meetingEdit',compact('meeting','team','team_length','team_chk_arr','team_chk_length'));
     }
 
     /**
@@ -111,7 +119,46 @@ class MeetingController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $name = $request->input('name');
+        $content = $request->input('content');
+        $meeting_date = $request->input('meeting_date');
+        $meeting_start = $request->input('meeting_start');
+        $meeting_end = $request->input('meeting_end');
+        $upload_date = $request->input('upload_date');
+        $upload_time = $request->input('upload_time');
+        $team = $request->input('team');
+        $TS = $request->input('TS');
+        $PA = $request->input('PA');
+        $team_limit = $request->input('team_limit');
+        $team_bonus = $request->input('team_bonus');
+        $member_limit = $request->input('member_limit');
+        $member_bonus = $request->input('member_bonus');
+
+        $team_length = count($team);
+        $team_chk = "";
+        for($i=0; $i<$team_length; $i++){
+            $team_chk = $team_chk . ' ' . $team[$i];
+        }
+
+        $meeting = Meeting::where('id','=',$id)->first();
+        $meeting -> name = $name;
+        $meeting -> meeting_date = $meeting_date;
+        $meeting -> meeting_start = $meeting_start;
+        $meeting -> meeting_end = $meeting_end;
+        $meeting -> content = $content;
+        $meeting -> upload_date = $upload_date;
+        $meeting -> upload_time = $upload_time;
+        $meeting -> report_team = $team_chk;
+        $meeting -> PA = $PA;
+        $meeting -> TS = $TS;
+        $meeting -> team_limit = $team_limit;
+        $meeting -> team_bonus = $team_bonus;
+        $meeting -> member_limit = $member_limit;
+        $meeting -> member_bonus = $member_bonus;
+        $meeting -> save();
+
+        $meeting = Meeting::all()->toArray();
+        return view('teacher_frontend.meeting',compact('meeting'));
     }
 
     /**
---
+++ resources/views/teacher_frontend/meetingEdit.blade.php
@@ -0,0 +1,159 @@
+@extends('teacher_frontend.layouts.master')
+@section('content')
+    <div class="content-page">
+        <!-- Start content -->
+        <div class="content">
+            <div class="container-fluid">
+
+                <!-- Page-Title -->
+                <div class="row">
+                    <div class="col-sm-12">
+                        <h4 class="page-title">編輯會議</h4>
+                        <h5 class="star">注意！「*」為必填欄位</h5>
+
+                        <form action="{{route('meeting.update',$meeting['id'])}}" method="post">
+                            {{ csrf_field() }}
+                            <p class="little-title">基本設定</p>
+                            <div class="col-md-8">
+                                <div class="form-group row">
+                                    <label for="name" class="col-md-2 control-label form-title">會議名稱*</label>
+                                    <div class="col-md-8">
+                                        <input type="text" class="form-control" id="name" name="name" required="" value="{{$meeting['name']}}">
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="content" class="col-md-2 control-label form-title">會議摘要*</label>
+                                    <div class="col-md-8">
+                                        <textarea id="content" name="content" class="form-control" maxlength="225" rows="2">{{$meeting['content']}}</textarea>
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="datepicker" class="col-md-2 control-label form-title">會議日期*</label>
+                                    <div class="col-md-8" >
+                                        <div class="input-group">
+                                            <input type="text" class="form-control" placeholder="yyyy-mm-dd" id="datepicker" name="meeting_date" required="" value="{{$meeting['meeting_date']}}">
+                                            <div class="input-group-append">
+                                                <span class="input-group-text bg-primary b-0 text-white"><i class="ti-calendar"></i></span>
+                                            </div>
+                                        </div><!-- input-group -->
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="meetingStart" class="col-md-2 control-label form-title">會議時間(起)*</label>
+                                    <div class="col-md-8">
+                                        <div class="input-group">
+                                            <input id="timepicker-start" type="text" class="form-control" name="meeting_start" required="" value="{{$meeting['meeting_start']}}">
+                                            <div class="input-group-append">
+                                                <span class="input-group-text bg-primary text-white"><i class="zmdi zmdi-time"></i></span>
+                                            </div>
+                                        </div><!-- input-group -->
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="meetingStart" class="col-md-2 control-label form-title">會議時間(迄)*</label>
+                                    <div class="col-md-8">
+                                        <div class="input-group">
+                                            <input id="timepicker-end" type="text" class="form-control" name="meeting_end" required="" value="{{$meeting['meeting_end']}}">
+                                            <div class="input-group-append">
+                                                <span class="input-group-text bg-primary text-white"><i class="zmdi zmdi-time"></i></span>
+                                            </div>
+                                        </div><!-- input-group -->
+                                    </div>
+                                </div>
+                                <div class="form-group row">
+                                    <label for="meetingStart" class="col-md-2 control-label form-title">繳交報告期限*</label>
+                                    <div class="col-md-8">
+                                        <div class="input-group m-b-10">
+                                            <input type="text" class="form-control" placeholder="yyyy-mm-dd" id="datepicker-upload" name="upload_date" required="" value="{{$meeting['upload_date']}}">
+                                            <div class="input-group-append">
+                                                <span class="input-group-text bg-primary b-0 text-white"><i class="ti-calendar"></i></span>
+                                            </div>
+                                        </div><!-- input-group -->
+                                        <div class="input-group">
+                                            <input id="timepicker-upload" type="text" class="form-control" name="upload_time" value="{{$meeting['upload_time']}}">
+                                            <div class="input-group-append">
+                                                <span class="input-group-text bg-primary text-white"><i class="zmdi zmdi-time"></i></span>
+                                            </div>
+                                        </div><!-- input-group -->
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="little-title">報告組別 *</p>
+                            <div class="col-md-8 p-b-10">
+                                @foreach($team as $row)
+                                    <input type="checkbox" id="team{{$row['id']}}" name="team[]" value="{{$row['name']}}"
+                                           @for($i=1; $i<$team_chk_length; $i++)
+                                                @if($team_chk_arr[$i] == $row['name'])
+                                                    checked
+                                                @endif
+                                            @endfor>
+                                    <label for="team{{$row['id']}}" class="control-label  form-title p-r-10">{{$row['name']}}</label>
+                                @endforeach
+                            </div>
+
+                            <p class="little-title">比重設定</p>
+                            <div class="col-md-8">
+                                <div class="input-group">
+                                    <label for="TS" class="col-md-2 control-label form-title">教師評分比重*</label>
+                                    <div class="form-group m-r-10">
+                                        <input type="text" class="form-control" id="TS" name="TS" style="text-align: center" required="" value="{{$meeting['TS']}}">
+                                    </div>
+                                    <label for="member_limit" class="form-title m-r-10">%</label>
+                                </div>
+                                <div class="input-group">
+                                    <label for="PA" class="col-md-2 control-label form-title">學生互評比重*</label>
+                                    <div class="form-group m-r-10">
+                                        <input type="text" class="form-control" id="PA" name="PA" style="text-align: center" required="" value="{{$meeting['PA']}}">
+                                    </div>
+                                    <label for="member_limit" class="form-title m-r-10">%</label>
+                                </div>
+                            </div>
+
+                            <p class="little-title">加分條件 *</p>
+                            <h5><b>評分組別</b></h5>
+                            <div class="col-md-8">
+                                <div class="input-group">
+                                    <label for="team_limit" class="col-md-2 control-label form-title">最多評分</label>
+                                    <div class="form-group m-r-10">
+                                        <input type="text" class="form-control" id="team_limit" name="team_limit" style="text-align: center" required="" value="{{$meeting['team_limit']}}">
+                                    </div>
+                                    <label for="team_limit" class="form-title m-r-10">份</label>
+                                </div>
+                                <div class="input-group">
+                                    <label for="team_bonus" class="col-md-2 control-label form-title">每評分一份 +</label>
+                                    <div class="form-group m-r-10">
+                                        <input type="text" class="form-control" id="team_bonus" name="team_bonus" style="text-align: center" required="" value="{{$meeting['team_bonus']}}">
+                                    </div>
+                                    <label for="team_bonus" class="form-title m-r-10">分</label>
+                                </div>
+                            </div>
+                            <h5><b>評分組員</b></h5>
+                            <div class="col-md-8">
+                                <div class="input-group">
+                                    <label for="member_limit" class="col-md-2 control-label form-title">最多評分</label>
+                                    <div class="form-group m-r-10">
+                                        <input type="text" class="form-control" id="member_limit" name="member_limit" style="text-align: center" required="" value="{{$meeting['member_limit']}}">
+                                    </div>
+                                    <label for="member_limit" class="form-title m-r-10">份</label>
+                                </div>
+                                <div class="input-group">
+                                    <label for="member_bonus" class="col-md-2 control-label form-title">每評分一份 +</label>
+                                    <div class="form-group m-r-10">
+                                        <input type="text" class="form-control" id="member_bonus" name="member_bonus" style="text-align: center" required="" value="{{$meeting['member_bonus']}}">
+                                    </div>
+                                    <label for="member_bonus" class="form-title m-r-10">分</label>
+                                </div>
+                            </div>
+
+                            <div class="m-t-30" align="right">
+                                <button type="submit" class="btn btn-success waves-effect waves-light button-font">確認</button>
+                            </div><!-- end col -->
+                            <input type="hidden" name="_method" value="PUT">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        </form>
+                    </div>
+                </div> <!-- container-fluid -->
+            </div> <!-- content -->
+        </div>
+@endsection
+@section('title','編輯會議')
