806fe49b35a4cb8f1a1d2399a9e87c064986ed0f
Fong-Yun Zhang <4a790071@stust.edu.tw>
2020-09-13 03:32:30 +0800

    新增結算成績按鈕

--- resources/views/teacher_frontend/meeting.blade.php
+++ resources/views/teacher_frontend/meeting.blade.php
@@ -21,9 +21,10 @@
                                     <th width="5%">#</th>
                                     <th width="25%">會議名稱</th>
                                     <th width="25%">會議日期</th>
-                                    <th width="10%">進入評分</th>
                                     <th width="10%">詳情</th>
                                     <th width="10%">刪除</th>
+                                    <th width="10%">進入評分</th>
+                                    <th width="10%">結算成績</th>
                                 </tr>
                                 </thead>
                                 <tbody>
@@ -32,13 +33,14 @@
                                         <td>{{$row['id']}}</td>
                                         <td>{{$row['name']}}</td>
                                         <td>{{$row['meeting_date']."　".date("H : i",strtotime($row['meeting_start'])). " ~ " .date("H : i",strtotime($row['meeting_end']))}}</td>
-                                        <td><a href="{{action('MeetingController@scoring_page',$row['id'])}}" class="btn btn-icon waves-effect waves-light btn-primary"><i class="fa fa-sign-in"></i></a></td>
                                         <td><a href="{{route('meeting.show',$row['id'])}}" class="btn btn-icon waves-effect waves-light btn-info"><i class="zmdi zmdi-info-outline"></i></a></td>
                                         <form action="{{route('meeting.destroy',$row['id'])}}" method="post">
                                             <td><button type="submit" class="btn btn-icon waves-effect waves-light btn-danger m-b-5" onclick="return(confirm('是否刪除此筆資料？'))"> <i class="fa fa-remove"></i> </button></td>
                                             <input type="hidden" name="_method" value="DELETE">
                                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
                                         </form>
+                                        <td><a href="{{action('MeetingController@scoring_page',$row['id'])}}" class="btn btn-icon waves-effect waves-light btn-primary"><i class="fa fa-sign-in"></i></a></td>
+                                        <td><button type="submit" class="btn btn-icon waves-effect waves-light btn-success"  id="stu_edit"><i class="fa fa-file-o"></i></button></td>
                                     </tr>
                                 @endforeach
                                 </tbody>
