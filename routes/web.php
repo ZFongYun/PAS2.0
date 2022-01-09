bc65b787314d8aeaf987ca59ed1ea6f1cdb72568
Fong-Yun Zhang <4a790071@stust.edu.tw>
2022-01-09 18:07:47 +0800

    Delete teacher's scoring route, page, controller.

--- routes/web.php
+++ routes/web.php
@@ -46,14 +46,7 @@ Route::prefix('APS_teacher')->group(function (){
 
         //====會議活動====
         Route::resource('meeting','MeetingController');
-        Route::prefix('meeting')->group(function (){
-            Route::get('score/{id}','MeetingController@scoring_page');
-            Route::post('score','MeetingController@score');
-            Route::post('scoring_team','MeetingController@scoring_team');
-            Route::post('edit_team','MeetingController@edit_team');
-            Route::post('scoring_stu','MeetingController@scoring_stu');
-            Route::post('edit_stu','MeetingController@edit_stu');
-        });
+
         Route::prefix('score')->group(function(){
             Route::post('/','ScoreController@scoring');
         });
