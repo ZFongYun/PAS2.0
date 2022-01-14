fff69fc7ae4fb29ccdd90d68304df6533dba8d3c
Fong-Yun Zhang <4a790071@stust.edu.tw>
2022-01-15 04:10:54 +0800

    Create leaderboard in student's index page.

--- app/Http/Controllers/StuIndexController.php
+++ app/Http/Controllers/StuIndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Bulletin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class StuIndexController extends Controller
 {
@@ -15,7 +16,68 @@ class StuIndexController extends Controller
     public function index()
     {
         $bulletin = Bulletin::all()->toArray();
-        return view('student_frontend.index',compact('bulletin'));
+        $user_id = auth('student')->user()->id;
+
+        // 尋找使用者的目前組別
+        $user_team = DB::Table('team_member')
+            ->join('team','team_member.team_id','team.id')
+            ->whereNull('team_member.deleted_at')
+            ->whereNull('team.deleted_at')
+            ->where('team_member.student_id',$user_id)
+            ->where('team.status',0)
+            ->select('team_member.team_id','team.name')
+            ->get()->toArray();
+
+        // 符合最晚完成結算+使用者目前組別的會議
+        $meeting = DB::table('meeting_team')
+            ->join('meeting','meeting_team.meeting_id','=','meeting.id')
+            ->join('team','meeting_team.team_id','=','team.id')
+            ->whereNull('meeting_team.deleted_at')
+            ->whereNull('meeting.deleted_at')
+            ->whereNull('team.deleted_at')
+            ->where('team.id',$user_team[0]->team_id)
+            ->where('meeting_team.calc_status','1')
+            ->orderBy('meeting_team.updated_at', 'desc')
+            ->select('meeting_team.team_id','meeting_team.meeting_id','meeting_team.updated_at')
+            ->get()->toArray();
+
+        //小組成員
+        $team_member = DB::Table('team_member')
+            ->join('student','team_member.student_id','student.id')
+            ->whereNull('team_member.deleted_at')
+            ->whereNull('student.deleted_at')
+            ->where('team_member.team_id',$user_team[0]->team_id)
+            ->select('team_member.*','student.name')
+            ->get()->toArray();
+
+        $stu_score_arr = array();
+        for ($i = 0; $i < count($team_member); $i++) {
+            $stu_score = DB::Table('student_score')
+                ->join('student', 'student_score.student_id', '=', 'student.id')
+                ->where('student_score.student_id', $team_member[$i]->student_id)
+                ->where('student_score.meeting_id', $meeting[0]->meeting_id)
+                ->select('student_score.*', 'student.name')
+                ->get()->toArray();  //組員成績
+            array_push($stu_score_arr, $stu_score);
+        }
+
+//        $tmp = 0;
+//        $stu_score_arr = array();
+//        for ($i=0; $i < count($stu_score_tmp); $i++){
+//            if ($stu_score_tmp[$i][0]->total < $tmp){
+//                array_push($stu_score_arr,$stu_score_tmp[$i][0]);
+//
+//            }else{
+//                $tmp = $stu_score_tmp[$i][0]->total;
+//
+//            }
+//        }
+        $user_team_name = $user_team[0]->name;
+        $score_record_date = date('Y/m/d', strtotime($meeting[0]->updated_at));
+
+//        dd($stu_score_arr);
+
+        return view('student_frontend.index',compact('bulletin','user_team_name','score_record_date','stu_score_arr'));
     }
 
     /**
--- resources/views/student_frontend/index.blade.php
+++ resources/views/student_frontend/index.blade.php
@@ -8,6 +8,50 @@
                 <!-- Page-Title -->
                 <div class="row">
                     <div class="col-sm-12">
+                        <h4 class="page-title p-b-10">排行榜</h4>
+                        <p>目前已加入組別：{{$user_team_name}}</p>
+                        <p>成績紀錄日期：{{$score_record_date}}</p>
+                        <div class="col-lg-10">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="table-responsive">
+                                        <table class="table m-0" style="text-align: center">
+                                            <thead>
+                                            <tr>
+                                                <th>排名</th>
+                                                <th>姓名</th>
+                                                <th>總得分</th>
+                                                <th>=</th>
+                                                <th>貢獻度</th>
+                                                <th>x</th>
+                                                <th>成效分數</th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            @for($i=0; $i<count($stu_score_arr); $i++)
+                                                <tr>
+                                                    <td>第{{$i+1}}名</td>
+                                                    @if(auth('student')->user()->name == $stu_score_arr[$i][0]->name)
+                                                        <td style="color: crimson; background: yellow">{{$stu_score_arr[$i][0]->name}}</td>
+                                                    @else
+                                                        <td>{{$stu_score_arr[$i][0]->name}}</td>
+                                                    @endif
+                                                        <td>{{$stu_score_arr[$i][0]->total}}</td>
+                                                    <td></td>
+                                                    <td>{{$stu_score_arr[$i][0]->CV}}</td>
+                                                    <td></td>
+                                                    <td>{{$stu_score_arr[$i][0]->EV}}</td>
+                                                </tr>
+                                            @endfor
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-sm-12">
                         <h4 class="page-title p-b-10">會議公告</h4>
                         @foreach($bulletin as $row)
                         <div class="col-lg-8">
